0bf2f386e964311c133203de30336698bca98e3f
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2020-11-09 20:36:38 +0200

    starting library

---
+++ makedalytics.py
@@ -0,0 +1,5 @@
+def remind_ds_libraries():
+    print ("Basics: \n import pandas as pd\n import numpy as np\n import matplotlib \n import matplotlib.pyplot as plt\n")
+    print("Visualization:\n from IPython.display import Markdown, display \n from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot\n import plotly.graph_objs as go\n import seaborn as sns")
+    print("NLP: import re\n import nltk \nfrom nltk.stem import SnowballStemmer \nfrom nltk.stem import WordNetLemmatizer \nfrom nltk import wordpunct_tokenize \n from nltk.corpus import stopwords \n from nltk.tokenize import sent_tokenize, word_tokenize \n from collections import Counter ")
+    print("Stats: \n import scipy \n from scipy import stats as st\n")
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="makedalytics",
+    version='0.0.1',
+    description="Python library to simpify data analytics",
+    author='doctormakeda@gmail.com',
+    author_email='doctormakeda@gmail.com',
+    maintainer='doctormakeda@gmail.com',
+    maintainer_email= 'doctormakeda@gmail.com',
+    url="https://github.com/todo",
+    license="MIT",
+    py_modules=["makedalytics"],
+    
+)
