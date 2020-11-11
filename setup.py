bec461f975cc7868dbb1d00ecde2aeb0eaac8e45
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2020-11-11 15:06:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='doctormakeda@gmail.com',
     maintainer='doctormakeda@gmail.com',
     maintainer_email= 'doctormakeda@gmail.com',
-    url="https://github.com/todo",
+    url="https://github.com/drcandacemakedamoore/makedalytics/blob/master/",
     license="MIT",
     py_modules=["makedalytics"],
     
