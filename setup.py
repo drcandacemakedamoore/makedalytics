c4be888ed5fb6008f76f49077dc14c9cacc365d9
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2020-11-14 11:46:18 +0200

    new setup version 0.0.7

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="makedalytics",
-    version='0.0.6',
-    description="Python library to simpify data analytics",
+    version='0.0.7',
+    description="Python library for reproducible data analytics of dataframes, text and images",
     author='doctormakeda@gmail.com',
     author_email='doctormakeda@gmail.com',
     maintainer='doctormakeda@gmail.com',
     maintainer_email= 'doctormakeda@gmail.com',
-    url="https://github.com/drcandacemakedamoore/makedalytics/blob/master/",
+    url="https://github.com/drcandacemakedamoore/makedalytics",
     license="MIT",
     py_modules=["makedalytics"],
     install_requires=["pip"]
