96fa2c1a5ebf1a9b941e499cc4a4741883efb874
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2021-04-21 19:33:05 +0300

    start testing file

--- test/test_makedalytics.py
+++ test/test_makedalytics.py
@@ -4,11 +4,20 @@ import pandas as pd
 import numpy as np
 import pytest
 import os
+import makedalytics
+
+the_directory =  os.path.join(os.path.dirname(__file__), 'directory')
+
+# def test_show_duplicates():
+#     test_dfE = (os.path.join(the_directory,'test_sample_df.csv'))
+#     test_df = pd.read_csv(test_dfE)
+#     checked_example = makedalytics.show_duplicates(test_df)
+#     assert len(checked_example) > 1 
 
 
-def test_show_duplicates():
-    test_dfE = (os.path.join(image_directory,'test_sample_df.csv'))
-    test_df = pd.read_csv(test_dfE)
-    checked_example = makedalytics.show_duplicates(test_df)
-    assert len(checked_example) > 1 
+
+def test_create_matrix():
+    moko = makedalytics.create_matrix(5, 5, 5)
+    # In python Sequence * Number = Sequence repeated Number of times
+    assert len(moko[0:1]) == 1    
 
